6613fe1672f33ba092be40d4b86eec05d1a0f11f
julien <julien.vesy@gmail.com>
2015-01-24 23:22:37 +0800

    beginning parsing network

--- ex2/sysinfo.c
+++ ex2/sysinfo.c
@@ -74,11 +74,106 @@ int	get_info_from_cpuinfo(char *src, char *buf, char *key)
 {
 }*/
 
+void	get_next_number_pos(char *buf, int *i)
+{
+  while (buf[*i] != ' ')
+    {
+      ++*i;
+    }
+  while (buf[*i] == ' ')
+    {
+      ++*i;
+    }
+}
+
+void	get_network_from_netdev(char *buf)
+{
+  int	i;
+  int	j;
+  int	n;
+  int	e;
+  int	c;
+  char	tab[8][3][64];
+
+  i = 0;
+  while (buf[i] != '\n' && buf[i] != '\0')
+    {
+      ++i;
+    }
+  while (buf[i] != '\n' && buf[i] != '\0')
+    {
+      ++i;
+    }
+  printk(KERN_INFO "avant grand while\n");
+  j = 0;
+  while (buf[i] != '\0')
+    {
+      n = 0;
+      while (buf[i] != '\n' && buf[i] != '\0')
+	{
+	  if (n == 0)
+	    {
+	      while (buf[i] == ' ' && buf[i] != '\0')
+		{
+		  ++i;
+		}
+	      e = 0;
+	      memset(tab[j][0], 0, 64);
+	      while (buf[i] != ':' && buf[i] != '\0')
+		{
+		  tab[j][0][e] = buf[i];
+		  ++e;
+		  ++i;
+		}
+	      printk(KERN_INFO "0 %s\n", tab[j][0]);
+	      while (buf[i] == ' ' && buf[i] != '\0')
+		{
+		  ++i;
+		}
+	      e = 0;
+	      memset(tab[j][1], 0, 64);
+	      while (buf[i] != ' ' && buf[i] != '\0')
+		{
+		  tab[j][1][e] = buf[i];
+		  ++e;
+		  ++i;
+		}
+	      printk(KERN_INFO "1 %s\n", tab[j][1]);
+	      while (buf[i] == ' ' && buf[i] != '\0')
+		{
+		  ++i;
+		}
+	      c = 2;
+	      while (c != 9)
+		{
+		  get_next_number_pos(buf, &i);
+		  ++c;
+		}
+	      e = 0;
+	      memset(tab[j][0], 0, 64);
+              while (buf[i] != ' ' && buf[i] != '\0')
+                {
+                  tab[j][2][e] = buf[i];
+                  ++e;
+                  ++i;
+                }
+	      printk(KERN_INFO "2 %s\n", tab[j][2]);
+	      --i;
+	      n = 1;
+	    }
+	  ++i;
+	  ++j;
+	}
+      ++i;
+    }
+  //  return (tab);
+}
+
 static int		__init sysinfo_init(void)
 {
   const char		*cpuinfo = "/proc/cpuinfo";
   //const char		*cpuusage = "/proc/stat";
-  //const char		*networkinfo = "/proc/net/dev";
+  const char		*networkinfo = "/proc/net/dev";
   struct sysinfo	si;
   struct file		*ffd;
   char			*buf;
@@ -94,14 +189,20 @@ static int		__init sysinfo_init(void)
   printk(KERN_INFO "%s\n", str);
   get_info_from_cpuinfo(buf, str, "cpu MHz"); 
   printk(KERN_INFO "%s\n", str);
-  get_info_from_cpuinfo(buf, str, "cache size"); 
+  get_info_from_cpuinfo(buf, str, "cache size");
   printk(KERN_INFO "%s\n", str);
+  file_close(ffd);
+
+  memset(buf, 0, 1024);
+  ffd = file_open(networkinfo, O_RDONLY, 0);
+  file_read(ffd, ffd->f_pos, buf, 1023);
+  get_network_from_netdev(buf);
+
   si_meminfo(&si);
   printk(KERN_INFO "total memory\t: %lu Mo\n", si.totalram);
   printk(KERN_INFO "free memory\t: %lu Mo\n", si.freeram);
   printk(KERN_INFO "shared memory\t: %lu Mo\n", si.sharedram);
   printk(KERN_INFO "uptime\t\t: %lu seconds\n", si.uptime);
-  file_close(ffd);
   kfree(buf);
   return (0);
 }
